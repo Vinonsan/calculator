47a81c2f8aa6f83378f3b74ba92ac8ce9fb2f12a
Vinonsan <vinonsan.99@gmail.com>
2025-06-14 17:16:25 +0530

    my latest

--- calculator/src/Cal.jsx
+++ calculator/src/Cal.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Cal() {
   return (
-    <div>Calculator</div>
+    <div>
+        calculator
+        <h1>this is my projectr</h1>
+        <p>this is the simple clculator</p>
+    </div>
   )
 }
 
